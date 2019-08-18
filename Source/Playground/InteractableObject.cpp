2f2a6e70f55908de9d4cf46daa15fb74d81794d6
Jonas Andersen <jonas.bjorge.andersen@gmail.com>
2019-08-18 15:23:25 +0200

    Alot of refactoring with traps and doors. A few new sounds, fixed camera

--- Source/Playground/InteractableObject.cpp
+++ Source/Playground/InteractableObject.cpp
@@ -10,7 +10,7 @@
 // Sets default values
 AInteractableObject::AInteractableObject()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Scene Component"));
@@ -26,6 +26,9 @@ void AInteractableObject::BeginPlay()
 	PrivateStartLocation = MoveableMesh->GetComponentLocation();
 
 	PrivateEndLocation = PrivateStartLocation + EndLocation;
+
+	OnOpenDelegate.AddDynamic(this, &AInteractableObject::Open);
+	OnClosedDelegate.AddDynamic(this, &AInteractableObject::Closed);
 }
 
 void AInteractableObject::Tick(float DeltaTime)
@@ -42,13 +45,47 @@ void AInteractableObject::Tick(float DeltaTime)
 	}
 	if (AllButtonsPressed)
 		OnButtonsPressed();
-	else
+	else if (!AllButtonsPressed)
 		OnButtonsReleased();
+}
+
+void AInteractableObject::Open()
+{
+	UE_LOG(LogTemp, Warning, TEXT("IS OPEN"));
+}
+
+void AInteractableObject::Closed()
+{
+	UE_LOG(LogTemp, Warning, TEXT("IS CLOSE"));
 }
 
 void AInteractableObject::UpdateMoveableObjectLocation(float Alpha)
 {
-	auto NewLocation = FMath::Lerp(PrivateStartLocation, PrivateEndLocation, FMath::Clamp<float>(Alpha, 0,1));
+	auto NewLocation = FMath::Lerp(PrivateStartLocation, PrivateEndLocation, FMath::Clamp<float>(Alpha, 0, 1));
 
 	MoveableMesh->SetWorldLocation(NewLocation);
+}
+
+void AInteractableObject::TimelineFinished(ETimelineDirection::Type Direction)
+{
+	switch (Direction)
+	{
+	case ETimelineDirection::Forward:
+		if (!bOpen)
+		{
+			bOpen = true;
+			OnOpenDelegate.Broadcast();
+
+			bClose = false;
+		}
+		break;
+	case ETimelineDirection::Backward:
+		if (!bClose)
+		{
+			bClose = true;
+			OnClosedDelegate.Broadcast();
+			bOpen = false;
+		}
+		break;
+	}
 }
--- Source/Playground/InteractableObject.h
+++ Source/Playground/InteractableObject.h
@@ -4,7 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/TimelineComponent.h"
 #include "InteractableObject.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpen);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnClosed);
+
 
 UCLASS()
 class PLAYGROUND_API AInteractableObject : public AActor
@@ -17,31 +22,53 @@ public:
 	UPROPERTY(VisibleAnywhere)
 		USceneComponent* SceneComponent;
 
-	UPROPERTY(VisibleAnywhere, Category = Moveable)
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = Moveable)
 		UStaticMeshComponent* MoveableMesh;
 
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Test")
+		FOnOpen OnOpenDelegate;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Test")
+		FOnClosed OnClosedDelegate;
+	
 	UFUNCTION(BlueprintImplementableEvent)
 		void OnButtonsPressed();
+
 	UFUNCTION(BlueprintImplementableEvent)
 		void OnButtonsReleased();
 
+	UFUNCTION()
+		void Open();
+
+	UFUNCTION()
+		void Closed();
+
+
 	UFUNCTION(BlueprintCallable)
 		void UpdateMoveableObjectLocation(float Alpha);
+
+	UFUNCTION(BlueprintCallable)
+		void TimelineFinished(ETimelineDirection::Type Direction);
 
 protected:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, Category = Settings)
 		TArray<class AGhostButton*> Buttons;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Moveable, Meta = (MakeEditWidget = true))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Settings, Meta = (MakeEditWidget = true))
 		FVector EndLocation;
 
-	UPROPERTY(BlueprintReadWrite, Category = Moveable)
+	UPROPERTY(BlueprintReadWrite, Category = Settings)
 		FVector PrivateStartLocation;
 
-	UPROPERTY(BlueprintReadWrite, Category = Moveable)
+	UPROPERTY(BlueprintReadWrite, Category = Settings)
 		FVector PrivateEndLocation;
 
+
+private:
+
+	bool bOpen = false;
+	bool bClose = true;
 };
